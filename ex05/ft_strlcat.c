4be27c0140b6be0c60e0406d41450b0d222f15ad
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2024-07-20 18:10:06 +0300

    fixed 05

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -14,13 +14,17 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	j;
+	unsigned int	c;
 	unsigned int	sum;
 
 	j = 0;
 	i = 0;
+	c = 0;
 	while (dest[i] != '\0')
 		i++;
-	sum = i + size;
+	while (src[c] != '\0')
+		c++;
+	sum = i + c;
 	while (src[j] != '\0' && i < size)
 	{
 		dest[i] = src[j];
